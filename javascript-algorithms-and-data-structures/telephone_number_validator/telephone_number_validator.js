e06890ba3316237d80ea7b0645a4520e0ffa44de
marcello <marcello.frattini7@gmail.com>
2021-09-18 07:13:26 +0200

    fix: update telephone_number_validator solution that was left incomplete

--- javascript-algorithms-and-data-structures/telephone_number_validator/telephone_number_validator.js
+++ javascript-algorithms-and-data-structures/telephone_number_validator/telephone_number_validator.js
@@ -1,14 +1,13 @@
 function telephoneCheck(str) {
-  if (containsForbiddenCharacters(str)) {
+  if (
+    containsForbiddenCharacters(str) ||
+    !validFirstCharacter(str[0]) ||
+    !balancedBrackets(str) ||
+    !bracketsAllowed(str) ||
+    tooManyGroups(str)
+  ) {
     return false;
   }
-  if (!validFirstCharacter(str[0])) {
-    return false;
-  }
-  if (!balancedBrackets(str)) {
-    return false;
-  }
-
   str = keepOnlyNumbers(str);
   if (str.length == 10) {
     return true;
@@ -19,26 +18,50 @@ function telephoneCheck(str) {
   return false;
 }
 
+// a phone number can contain only numbers and [" ", "-", "(", ")"]
 function containsForbiddenCharacters(str) {
   const regex = new RegExp("[^(0-9\\s-\\(\\))]");
   return regex.test(str);
 }
 
+// the first character can be only a number or "("
 function validFirstCharacter(char) {
   const regex = new RegExp("[0-9\\(]");
   return regex.test(char);
 }
 
+// all "(" and ")" must be balanced
 function balancedBrackets(str) {
   let stack = [];
-  for (char of str) {
+  for (let char of str) {
     if (char == "(") {
       stack.push(char);
     } else if (char == ")") {
+      if (stack.length == 0) {
+        return false;
+      }
       stack.pop();
     }
   }
   return stack.length == 0;
+}
+
+// if present, brackets must contain 3 chars
+function bracketsAllowed(str) {
+  let open_bracket_idx = str.search("\\(");
+  let closed_bracket_idx = str.search("\\)");
+  if (open_bracket_idx == -1 || closed_bracket_idx == -1) {
+    return true;
+  }
+  if (closed_bracket_idx - open_bracket_idx == 4) {
+    return true;
+  }
+  return false;
+}
+
+// splitting over separators cannot produce more than 4 elements
+function tooManyGroups(str) {
+  return str.replaceAll("-", " ").split(" ").length > 4
 }
 
 function keepOnlyNumbers(str) {
